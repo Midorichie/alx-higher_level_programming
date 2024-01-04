fa82e205b511626db1c8607c3f35b63e70f9eced
Midorichie <hamsohood@gmail.com>
2024-01-04 11:51:11 +0100

    Arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} argumenrs:".format(x))
+    else:
+        print("{} arguments:".format(x))
+
+    for y in range(x):
+        print("{}: {:s}".format(y + 1, argv[y + 1]))
