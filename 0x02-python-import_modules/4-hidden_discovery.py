2f105dd640a05b50f64e5ad1bfaf09d1aa63295e
Midorichie <hamsohood@gmail.com>
2024-01-05 05:19:20 +0100

    Hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     from hidden_4 import *
     allf = dir()
     for n in range(0, len(allf)):
-        if allf[n] [:2] != "__":
+        if allf[n][:2] != "__":
             print("{:s}".format(allf[n]))
