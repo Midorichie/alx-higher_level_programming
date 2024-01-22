8f4b0ca6166b74ce14814a5bcdf95a7a82f3650c
Midorichie <hamsohood@gmail.com>
2024-01-22 14:54:22 +0100

    Safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[val], end='')
             count += 1
-        except indexError as e:
+        except IndexError as e:
             pass
 
-        print()
-        return count
+    print()
+    return count
