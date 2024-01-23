0334552cbf0a81cb68ee9f7462f14df3a29033a6
Midorichie <hamsohood@gmail.com>
2024-01-23 10:14:56 +0100

    Safe print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integer(my_list=[], x=0):
     count = 0
-    for element in range(x):
+    for m in range(x):
         try:
-            if isinstance(my_list[element], int):
-                print("{:d}".format(my_list[element]), end='')
-                count += 1
-        except ValueError:
+            print("{:d}".format(my_list[m]), end="")
+            count += 1
+        except (ValueError, TypeError):
             pass
-
     print()
-    return count
+    Return
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = None
     try:
-        result = a / b
-    except Exception:
-        pass
+        div = a / b
+    except (ZeroDivisionError):
+        div = None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {}".format(div))
+        return div
