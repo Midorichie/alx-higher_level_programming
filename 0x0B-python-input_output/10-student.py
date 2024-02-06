4729990259f19c467e6349395221ae83b2c7a7d3
Midorichie <hamsohood@gmail.com>
2024-02-07 00:29:44 +0100

    Update

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""10-student module"""
+
+
+class Student:
+    """A class student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """The class constructor.
+        Args:
+            first_name: the first name of the student
+            last_name: the last name of the student
+            age: the age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """The public method that retrieves a dictionary representation
+        of a student instance.
+        """
+        result = {}
+        if attrs is None:
+            for key in self.__dict__:
+                result[key] = getattr(self, key)
+
+        else:
+            for key in attrs:
+                if hasattr(self, key):
+                    result[key] = getattr(self, key)
+        return result
