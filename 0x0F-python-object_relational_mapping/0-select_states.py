e190835e123f5d5f83176ffa0634bd5f9947c64f
Midorichie <hamsohood@gmail.com>
2024-04-13 17:02:30 +0100

    Task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
-        port=3300,
+        port=3306,
         db=sys.argv[3]
     )
 
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     state_col = cur.fetchall()
 
     for row in state_col:
-        print("()".format(row))
+        print("{}".format(row))
 
     cur.close()
     db.close()
